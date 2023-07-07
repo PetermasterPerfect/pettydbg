c888d31e8a1dcd92e13b8adffdb309d0a4bfbd55
Petermaster <zxcv@asus_PC.localdomain>
2023-07-07 15:15:45 +0200

    remove winmake.bat

--- winmake.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem for /f %i in ('cd') do set dir=%i
-set dir=C:\Users\asus\Desktop\p.kajda\ReCraft\pettydbg
-cd c:\MinGW\bin
-g++ %dir%\main.cpp %dir%\debugger.cpp %dir%\commandline.cpp %dir%\splitstring.cpp %dir%\unicodeStringEx.cpp -o %dir%\dbg.exe -static -std=c++17 -I %dir%\capstone\include -L %dir%\capstone -lcapstone
-cd %dir%
